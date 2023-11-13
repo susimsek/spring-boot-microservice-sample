67c7de6ecd25f8d2924b6b7440276883259a2617
suayb <suayb.simsek@ykteknoloji.com.tr>
2023-11-13 12:28:05 +0300

    redis cache

--- card/src/main/java/io/github/susimsek/card/config/DatabaseConfig.java
+++ card/src/main/java/io/github/susimsek/card/config/DatabaseConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.auditing.DateTimeProvider;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import java.time.Clock;
@@ -11,6 +12,7 @@ import java.time.Instant;
 import java.util.Optional;
 
 @Configuration(proxyBeanMethods = false)
+@EnableJpaRepositories(basePackages = "io.github.susimsek.card.repository")
 @EnableJpaAuditing(
     dateTimeProviderRef = "dateTimeProvider",
     auditorAwareRef = "auditAwareImpl")
---
+++ loan/src/main/java/io/github/susimsek/loan/config/CacheConfig.java
@@ -0,0 +1,44 @@
+package io.github.susimsek.loan.config;
+
+
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.cfg.AvailableSettings;
+import org.redisson.api.RedissonClient;
+import org.redisson.hibernate.RedissonRegionFactory;
+import org.redisson.spring.cache.RedissonSpringCacheManager;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration(proxyBeanMethods = false)
+@ConditionalOnClass({RedissonClient.class})
+@EnableCaching
+public class CacheConfig {
+
+    @Bean
+    @ConditionalOnProperty("spring.jpa.properties.hibernate.cache.use_second_level_cache")
+    public HibernatePropertiesCustomizer hibernatePropertiesCustomizer(RedissonClient redissonClient) {
+        return hibernateProperties -> hibernateProperties.put(AvailableSettings.CACHE_REGION_FACTORY, new RedissonRegionFactory() {
+            @Override
+            protected RedissonClient createRedissonClient(StandardServiceRegistry registry, Map properties) {
+                return redissonClient;
+            }
+        });
+    }
+
+    @Bean
+    public CacheManager cacheManager(RedissonClient redissonClient) {
+        Map<String, org.redisson.spring.cache.CacheConfig> config = new HashMap<>();
+
+        // create "testMap" spring cache with ttl = 24 minutes and maxIdleTime = 12 minutes
+        config.put("testMap", new org.redisson.spring.cache.CacheConfig(24*60*1000, 12*60*1000));
+        return new RedissonSpringCacheManager(redissonClient, config);
+    }
+}
