846b375ad41d7d1fca686f950f619124494534ec
suayb <suayb.simsek@ykteknoloji.com.tr>
2023-11-18 16:04:24 +0300

    fix sonar issues

--- gateway-server/src/main/java/io/github/susimsek/gatewayserver/config/KeycloakRoleConverter.java
+++ gateway-server/src/main/java/io/github/susimsek/gatewayserver/config/KeycloakRoleConverter.java
@@ -2,6 +2,7 @@ package io.github.susimsek.gatewayserver.config;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import org.springframework.core.convert.converter.Converter;
@@ -12,15 +13,20 @@ import org.springframework.security.oauth2.jwt.Jwt;
 public class KeycloakRoleConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
 
     @Override
+    @SuppressWarnings("unchecked")
     public Collection<GrantedAuthority> convert(Jwt jwt) {
+
         final Map<String, Object> realmAccess = (Map<String, Object>) jwt.getClaims().get("realm_access");
-        var roles = (List<String>) realmAccess.get("roles");
-        Collection<GrantedAuthority> authorities = new ArrayList<>();
-        roles.stream()
-            .map(roleName -> "ROLE_" + roleName) // prefix to map to a Spring Security "role"
-            .map(SimpleGrantedAuthority::new)
-            .forEach(authorities::add);
-        return authorities;
+        if (realmAccess != null) {
+            var roles = (List<String>) realmAccess.get("roles");
+            Collection<GrantedAuthority> authorities = new ArrayList<>();
+            roles.stream()
+                .map(roleName -> "ROLE_" + roleName) // prefix to map to a Spring Security "role"
+                .map(SimpleGrantedAuthority::new)
+                .forEach(authorities::add);
+            return authorities;
+        }
+        return Collections.emptyList();
     }
 
 }
