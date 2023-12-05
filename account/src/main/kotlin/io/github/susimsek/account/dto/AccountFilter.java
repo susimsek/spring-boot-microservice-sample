aefc5210b1a9e23c693204518f17c5921532d52e
suayb <suayb.simsek@ykteknoloji.com.tr>
2023-12-05 21:09:11 +0300

    exception handler

---
+++ account/src/main/kotlin/io/github/susimsek/account/dto/AccountFilter.java
@@ -0,0 +1,49 @@
+package io.github.susimsek.account.dto;
+
+import io.github.susimsek.account.entity.Account;
+import io.github.susimsek.account.entity.Account_;
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import java.util.LinkedList;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.util.StringUtils;
+
+@Getter
+@Setter
+public class AccountFilter implements Specification<Account> {
+
+    private String accountType;
+    private String branchAddress;
+    private Boolean communicationSw;
+
+    @Override
+    public Predicate toPredicate(
+        Root<Account> root,
+        CriteriaQuery<?> query,
+        CriteriaBuilder criteriaBuilder) {
+        var predicates = new LinkedList<Predicate>();
+
+        if (StringUtils.hasText(accountType)) {
+            predicates.add(criteriaBuilder.equal(root.get(Account_.ACCOUNT_TYPE), accountType));
+        }
+
+        if (StringUtils.hasText(branchAddress)) {
+            predicates.add(criteriaBuilder.like(
+                criteriaBuilder.lower(
+                    root.get(Account_.BRANCH_ADDRESS)), branchAddress.toLowerCase() + "%"));
+        }
+
+        if (communicationSw != null) {
+            predicates.add(criteriaBuilder.equal(root.get(Account_.COMMUNICATION_SW), communicationSw));
+        }
+
+        if (predicates.isEmpty()) {
+            return null;
+        }
+        return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
+    }
+}
