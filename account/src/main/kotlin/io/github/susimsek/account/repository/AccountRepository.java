2bd034e5949a0b2327727816ea32ce9978591e9d
suayb <suayb.simsek@ykteknoloji.com.tr>
2023-12-06 08:40:34 +0300

    exception handler

--- account/src/main/kotlin/io/github/susimsek/account/repository/AccountRepository.java
+++ account/src/main/kotlin/io/github/susimsek/account/repository/AccountRepository.java
@@ -4,6 +4,7 @@ import io.github.susimsek.account.entity.Account;
 import java.util.Optional;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.repository.history.RevisionRepository;
 import org.springframework.stereotype.Repository;
@@ -11,7 +12,8 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long>,
-    RevisionRepository<Account, Long, Integer> {
+    RevisionRepository<Account, Long, Integer>,
+    JpaSpecificationExecutor<Account> {
     String ACCOUNT_BY_CUSTOMER_ID_CACHE = "accountByCustomerId";
 
     @Cacheable(cacheNames = ACCOUNT_BY_CUSTOMER_ID_CACHE)
