c9ab07cb0d236a2b4702872f9a43f8f2c23a1e11
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-04-12 13:42:29 +0300

    add api gateway logging

--- account/src/main/kotlin/io/github/susimsek/account/mapper/AccountMapper.java
+++ account/src/main/kotlin/io/github/susimsek/account/mapper/AccountMapper.java
@@ -2,8 +2,17 @@ package io.github.susimsek.account.mapper;
 
 import io.github.susimsek.account.dto.AccountDTO;
 import io.github.susimsek.account.entity.Account;
+import io.github.susimsek.account.mapper.util.AccountMappingUtil;
+import java.util.Map;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
-@Mapper(componentModel = "spring")
+@Mapper(uses = AccountMappingUtil.class)
 public interface AccountMapper extends EntityMapper<AccountDTO, Account> {
+
+
+    @Mapping(source = ".", target = "accountNumber", qualifiedBy = AccountMappingUtil.AccountNumber.class)
+    @Mapping(source = ".", target = "accountType", qualifiedBy = AccountMappingUtil.AccountType.class)
+    @Mapping(source = ".", target = "branchAddress", qualifiedByName = "branchAddress")
+    AccountDTO fromMap(Map<String, Object> map);
 }
---
+++ account/src/main/kotlin/io/github/susimsek/account/mapper/util/AccountMappingUtil.java
@@ -0,0 +1,40 @@
+package io.github.susimsek.account.mapper.util;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.util.Map;
+import org.mapstruct.Named;
+import org.mapstruct.Qualifier;
+
+public class AccountMappingUtil {
+
+    @Qualifier
+    @Target(ElementType.METHOD)
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface AccountNumber {
+    }
+
+    @Qualifier
+    @Target(ElementType.METHOD)
+    @Retention(RetentionPolicy.SOURCE)
+    public @interface AccountType {
+    }
+
+    @AccountNumber
+    public Long accountNumber(Map<String, Object> in) {
+        return (Long) in.get("accountNumber");
+    }
+
+    @AccountType
+    public String accountType(Map<String, Object> in) {
+        return (String) in.get("accountType");
+    }
+
+    @Named("branchAddress")
+    public String branchAddress(Map<String, Object> in) {
+        return (String) in.get("branchAddress");
+    }
+
+}
