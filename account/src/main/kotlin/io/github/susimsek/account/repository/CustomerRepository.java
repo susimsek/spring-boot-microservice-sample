8b7cccd37decef8252a72ed78f5b7d1d2fcdf4bb
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-04-25 22:12:29 +0300

    add api gateway logging

--- account/src/main/kotlin/io/github/susimsek/account/repository/CustomerRepository.java
+++ account/src/main/kotlin/io/github/susimsek/account/repository/CustomerRepository.java
@@ -1,12 +1,18 @@
 package io.github.susimsek.account.repository;
 
+import static org.hibernate.jpa.HibernateHints.HINT_CACHEABLE;
+
 import io.github.susimsek.account.entity.Customer;
+import jakarta.persistence.QueryHint;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    @QueryHints(@QueryHint(name = HINT_CACHEABLE, value = "true"))
     boolean existsByMobileNumber(String mobileNumber);
 
     Optional<Customer> findByMobileNumber(String mobileNumber);
