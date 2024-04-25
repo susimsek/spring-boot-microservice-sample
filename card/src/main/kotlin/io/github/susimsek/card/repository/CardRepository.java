8c1d78a1a1e473c7ade6c638065d3f2ed6a24bdf
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-04-25 22:14:13 +0300

    add api gateway logging

--- card/src/main/kotlin/io/github/susimsek/card/repository/CardRepository.java
+++ card/src/main/kotlin/io/github/susimsek/card/repository/CardRepository.java
@@ -1,6 +1,9 @@
 package io.github.susimsek.card.repository;
 
+import static org.hibernate.jpa.HibernateHints.HINT_CACHEABLE;
+
 import io.github.susimsek.card.entity.Card;
+import jakarta.persistence.QueryHint;
 import java.util.Optional;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Limit;
@@ -9,6 +12,7 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.domain.Window;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -20,6 +24,7 @@ public interface CardRepository extends JpaRepository<Card, Long>,
     @Cacheable(cacheNames = CARD_BY_MOBILE_NUMBER_CACHE)
     Optional<Card> findByMobileNumber(String mobileNumber);
 
+    @QueryHints(@QueryHint(name = HINT_CACHEABLE, value = "true"))
     boolean existsByMobileNumber(String mobileNumber);
 
     Optional<Card> findByCardNumber(String cardNumber);
--- loan/src/main/kotlin/io/github/susimsek/loan/repository/LoanRepository.java
+++ loan/src/main/kotlin/io/github/susimsek/loan/repository/LoanRepository.java
@@ -1,10 +1,14 @@
 package io.github.susimsek.loan.repository;
 
+import static org.hibernate.jpa.HibernateHints.HINT_CACHEABLE;
+
 import io.github.susimsek.loan.entity.Loan;
+import jakarta.persistence.QueryHint;
 import java.util.Optional;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -16,6 +20,7 @@ public interface LoanRepository extends JpaRepository<Loan, Long>,
     @Cacheable(cacheNames = LOAN_BY_MOBILE_NUMBER_CACHE)
     Optional<Loan> findByMobileNumber(String mobileNumber);
 
+    @QueryHints(@QueryHint(name = HINT_CACHEABLE, value = "true"))
     boolean existsByMobileNumber(String mobileNumber);
 
     Optional<Loan> findByLoanNumber(String loanNumber);
