c628228d68721a94937adc79ce04883d3341deba
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-05-01 20:53:36 +0300

    add api gateway logging

--- card/src/main/kotlin/io/github/susimsek/card/config/SecurityConfig.java
+++ card/src/main/kotlin/io/github/susimsek/card/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,7 +16,9 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
@@ -69,5 +72,14 @@ public class SecurityConfig {
     @Bean
     MvcRequestMatcher.Builder mvc(HandlerMappingIntrospector introspector) {
         return new MvcRequestMatcher.Builder(introspector);
+    }
+
+    @Bean
+    AuthenticationProvider authenticationProvider(
+        JwtDecoder jwtDecoder,
+        Converter<Jwt, AbstractAuthenticationToken> authenticationConverter) {
+        var  authenticationProvider = new JwtAuthenticationProvider(jwtDecoder);
+        authenticationProvider.setJwtAuthenticationConverter(authenticationConverter);
+        return authenticationProvider;
     }
 }
