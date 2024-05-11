8e882fe84c0e4a6e9fdc08713f3bf329c22ff60a
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-05-11 16:54:30 +0300

    add api gateway logging

--- card/src/main/kotlin/io/github/susimsek/card/config/Constants.java
+++ card/src/main/kotlin/io/github/susimsek/card/config/Constants.java
@@ -9,4 +9,6 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public final class Constants {
     public static final String SYSTEM = "system";
+
+    public static final String SPRING_PROFILE_DEVELOPMENT = "local";
 }
