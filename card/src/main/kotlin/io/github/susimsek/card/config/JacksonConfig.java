d97679afc3ad1b7ea05da6e039e570d643a0d492
suayb <suayb.simsek@ykteknoloji.com.tr>
2024-05-12 20:42:21 +0300

    add api gateway logging

---
+++ card/src/main/kotlin/io/github/susimsek/card/config/JacksonConfig.java
@@ -0,0 +1,14 @@
+package io.github.susimsek.card.config;
+
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration(proxyBeanMethods = false)
+public class JacksonConfig {
+
+    @Bean
+    public JavaTimeModule javaTimeModule() {
+        return new JavaTimeModule();
+    }
+}
